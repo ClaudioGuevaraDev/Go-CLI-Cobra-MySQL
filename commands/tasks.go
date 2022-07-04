1d9787839137d016e3d6c47ec228011aa699670e
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-07-04 10:39:28 -0400

    insert added

--- commands/tasks.go
+++ commands/tasks.go
@@ -1,10 +1,31 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/ClaudioGuevaraDev/Go-CLI-Cobra-MySQL/database"
+	"github.com/spf13/cobra"
+)
 
 var InsertTaskCommand = &cobra.Command{
 	Use:   "insert",
 	Short: "Insert a task",
+	Args:  cobra.MinimumNArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		title := args[0]
+		description := args[1]
+
+		sql := "INSERT INTO tasks (title, description) VALUES (?, ?)"
+		_, err := database.DB.Exec(sql, title, description)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Task created")
+	},
 }
 
 var ListTasksCommand = &cobra.Command{
